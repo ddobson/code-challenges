a2b3374c9fc6241f97ded14911e3e65dd41847d1
ddobson <ddobson445@gmail.com>
2017-07-18 13:00:22 -0400

    Cleanup isSquaredCopy tests

--- squared-copy/squared-copy.test.js
+++ squared-copy/squared-copy.test.js
@@ -1,11 +1,16 @@
 const isSquaredCopy = require('./squared-copy')
 
-test('#isSquaredCopy', () => {
+describe('#isSquaredCopy', () => {
   const arr1 = [121, 144, 19, 161, 19, 144, 19, 11]
   const arr2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
   const arr3 = [1, 2, 3, 4, 5, 6, 7, 8]
 
-  expect(isSquaredCopy(arr1, arr2)).toBe(true)
-  expect(isSquaredCopy(arr1, arr3)).toBe(false)
-  expect(isSquaredCopy(arr1, null)).toBe(false)
+  test('it finds squared copies', () => {
+    expect(isSquaredCopy(arr1, arr2)).toBe(true)
+    expect(isSquaredCopy(arr1, arr3)).toBe(false)
+  })
+
+  test('it hanles null values', () => {
+    expect(isSquaredCopy(arr1, null)).toBe(false)
+  })
 })
